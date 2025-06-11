454dbcd7298b96560320522b4dc5b911690a3deb
mykyta <akiplinn@gmail.com>
2025-06-11 17:00:11 +0200

    Completed about page

--- project-bitchboy/src/pages/AboutPage.jsx
+++ project-bitchboy/src/pages/AboutPage.jsx
@@ -5,31 +5,75 @@ import TextSection from "../components/jsx/TextSection.jsx";
 export default function AboutPage() {
     return (
         <div className="background-container">
+            <div className="background-label-wrapper">
+                <div className="background-label-main-layer">
+                    BLOCKSYSTEM
+                </div>
+                <div className="background-label-layer-1">
+                    BLOCKSYSTEM
+                </div>
+                <div className="background-label-layer-2">
+                    BLOCKSYSTEM
+                </div>
+                <div className="background-label-layer-3">
+                    BLOCKSYSTEM
+                </div>
+                <div className="background-label-layer-4">
+                    BLOCKSYSTEM
+                </div>
+                <div className="background-label-layer-5">
+                    BLOCKSYSTEM
+                </div>
+                <div className="background-label-layer-6">
+                    BLOCKSYSTEM
+                </div>
+                <div className="background-label-layer-7">
+                    BLOCKSYSTEM
+                </div>
+                <div className="background-label-layer-8">
+                    BLOCKSYSTEM
+                </div>
+            </div>
             <div className="person-right-image-wrapper">
-                <img
-                    className="person-image"
-                    src={"/images/about-page/person.jpg"}
-                    alt={""}
-                />
+                <a
+                    href="https://blocksystem.org/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img
+                        className="person-image"
+                        src={"/images/about-page/euri.jpeg"}
+                        alt={""}
+                    />
+                </a>
             </div>
             <div className="person-right-text-section">
                 <TextSection
-                    title={"CHRISTOS 2"}
+                    title={"EURI"}
                     text={"Interaction Tech @ UTwente & Creator @ BLOCK SYSTEM \n Lorem Ipsum"}
                 ></TextSection>
             </div>
             <div className="person-left-image-wrapper">
-                <img
-                    className="person-image"
-                    src={"/images/about-page/person.jpg"}
-                    alt={""}
-                />
+                <a
+                    href="https://christos.tech/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img
+                        className="person-image"
+                        src={"/images/about-page/christos2.png"}
+                        alt={""}
+                    />
+                </a>
             </div>
             <div className="person-left-text-section">
                 <TextSection
-                    title={"CHRISTOS 1"}
+                    title={"CHRISTOS"}
                     text={"Interaction Tech @ UTwente & Creator @ BLOCK SYSTEM \n Lorem Ipsum"}
                 ></TextSection>
+            </div>
+            <div className="team-label">
+                BITCHBOY TEAM
             </div>
             <div className="svg-bottom-wrapper">
                 <img
